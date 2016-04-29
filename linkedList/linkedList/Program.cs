dcebad853f5ecb855fd48411e5de63f02b36c206
Eric Gruber <lonephoenix@gmail.com>
2016-04-28 22:16:28 -0400

    Added AddRear method

--- linkedList/linkedList/Program.cs
+++ linkedList/linkedList/Program.cs
@@ -55,6 +55,46 @@ namespace linkedList
                 Console.WriteLine("Added new node with data: " + node.data);
             }
         }
+        public void AddEnd(Object incomingData)
+        {
+
+            var node = new Node();
+            node.data = incomingData;
+
+            // Now, I'm going to check to see if this is the first item we are inserting
+            if (head == null)
+            {
+                // Head hasn't been assigned, so assign it
+                head = node;
+                Console.WriteLine("Added first node with data: " + node.data);
+            }
+            else
+            {
+                // Create a cursor node to hold the data as I move through
+                var currentLocation = new Node();
+
+                currentLocation.next = head;
+
+                // There's a bit more grace in moving through the list a set number of times instead of 
+                for (int nodeCount = 0; nodeCount <= size; nodeCount++)
+                {
+                    if(currentLocation.next == null)
+                    {
+                        Console.WriteLine("Current Location's Data: " + currentLocation.data);
+                        currentLocation.next = node;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Current Location's Data: " + currentLocation.data);
+                        currentLocation = currentLocation.next;
+                    }
+                }
+
+                // The cursor is now at a position where the next node is null                
+                Console.WriteLine("Added new node with data: " + node.data);
+                size++;
+            }
+        }
         public void Length()
         {
             Console.WriteLine("The current length is: " + size);
@@ -63,21 +103,21 @@ namespace linkedList
         public void printNodes()
         {
             // Start at the head
-            Node current_location = head;
+            Node currentLocation = head;
             int nodeCount = 0;
             
             // If there isn't another node, just print this node and don't go into a loop.
-            if(current_location.next == null)
+            if(currentLocation.next == null)
             {
-                Console.WriteLine("Data: " + current_location.data);
+                Console.WriteLine("Data: " + currentLocation.data);
             }
             else
             {
                 // Cycle through nodes until the next node isn't null
-                while (current_location != null && nodeCount != size)
+                while (currentLocation != null && nodeCount != size)
                 {
-                    Console.WriteLine("Data: " + current_location.data);
-                    current_location = current_location.next;
+                    Console.WriteLine("Data: " + currentLocation.data);
+                    currentLocation = currentLocation.next;
 
                     // To ensure I'm not getting stuck in a loop, only proceed through the list up to the length 
                     nodeCount++;
@@ -107,6 +147,10 @@ namespace linkedList
             // Output the current size of the list
             list1.Length();
             list1.printNodes();
+
+            list1.AddEnd("Sun!");
+            list1.printNodes();
+            list1.Length();
 
             // Add a ReadKey just to see the output of a successful run
             Console.ReadKey();
