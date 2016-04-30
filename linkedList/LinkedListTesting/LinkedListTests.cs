74ae5d84b867a44dd7202f43123c90ef88085a69
Eric Gruber <lonephoenix@gmail.com>
2016-04-30 08:43:40 -0400

    Updated unit tests for AddFront and AddEnd Created unit test for printNodes

--- linkedList/LinkedListTesting/LinkedListTests.cs
+++ linkedList/LinkedListTesting/LinkedListTests.cs
@@ -17,14 +17,47 @@ namespace LinkedListTesting
             test_list.printFirstNode();
         }
         [TestMethod]
-        public void Add_New_Node_With_No_Content()
+        public void Add_Front_With_No_Content()
         {
             // Arrange
-            int data = 1;
             LL test_list = new LL();
 
             // Act
-            test_list.Add(data);
+            test_list.AddFront(null);
+        }
+        [TestMethod]
+        public void Add_Front_With_Content()
+        {
+            // Arrange 
+            string test_data = "test Data";
+            LL test_list = new LL();
+
+            // Act
+            test_list.AddFront(test_data);
+        }
+        [TestMethod]
+        public void Add_Back_With_Content()
+        {
+            // Arrange 
+            string test_data = "test data";
+            LL test_list = new LL();
+
+            // Act
+            test_list.AddEnd(test_data);
+
+        }
+        [TestMethod]
+        public void Verify_Print_Nodes_Outputs_All_Nodes_In_Order()
+        {
+            // Arrange
+            string testInputOne = "first";
+            string testInputTwo = "second";
+            LL test_list = new LL();
+            test_list.AddFront(testInputOne);
+            test_list.AddEnd(testInputTwo);
+
+            // Act
+            test_list.printNodes();
         }
     }
 }
